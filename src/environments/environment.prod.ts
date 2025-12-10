27764cc2233dca03274e27ba09df0c2d815dd4fc
imDark111 <jchugchil1@utmachala.edu.ec>
2025-12-10 01:57:52 -0500

    Config: Actualizar apiUrl con Railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'CAMBIAR_POR_URL_RAILWAY/api'  // Ejemplo: https://tu-app.railway.app/api
+  apiUrl: 'https://backend-paraisoverde-dashboard.up.railway.app/api'
 };
